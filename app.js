f6e8a74cca214bcf3cfd18b3252b9579b491996a
yingying air <yingyingfengg@gmail.com>
2022-04-09 09:46:57 -0400

    showing client info and html template engine

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const app = express();
 app.use(express.json());
 app.engine('html', require('ejs').renderFile);
 
+//^^ when render is called, when we are rendering an html file, we use this template function. this function takes a file and data and gives us back some html. we dont call it directly, we let express do it directly and express uses template engines to set it up. 
+// is called an html template engine
+
 const { models: { User }} = require('./db');
 const path = require('path');
 
-app.get('/', (req, res)=> res.render(path.join(__dirname, 'index.html'), { GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID }));
+app.get('/', (req, res)=> res.render(path.join(__dirname, 'index.html'), { GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID, FOO: Math.random() }));
+//^^ can add client info with foo and show it on the page inside html.js with foo under body
 
 app.get('/api/auth', async(req, res, next)=> {
   try {
